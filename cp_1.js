7d8d1a5740fc42a59233a08f723fbefd79e6d1e6
Iida Kenny <iida@usf.edu>
2026-03-13 21:53:45 -0400

    Created a test to console.log the users comments and added a dynamic character counter that changes color based on how many characters there are left.

---
+++ cp_1.js
@@ -0,0 +1,29 @@
+const form_input = document.getElementById('user_feedback_form');
+
+const comment_input = document.getElementById('comment');
+
+
+document.querySelector('#user_feedback_form').addEventListener('submit', (event) => {
+    event.preventDefault();
+    console.log(comment_input.value);
+});
+
+const character_counter = document.getElementById('char_counter');
+
+comment_input.addEventListener('input', () => {
+    const current_length = comment_input.value.length;
+    character_counter.textContent = `${current_length} / 500 characters`;
+
+    character_counter.classList.remove('warning', 'danger');
+
+    if (current_length > 400) {
+        character_counter.classList.add('warning');
+    }
+
+    if (current_length === 500) {
+        character_counter.classList.add('danger');
+    }
+});
+
+
+
